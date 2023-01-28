d6dbdbb493e377078b05235eb4334c32d8834f89
Robinee <yb6925@naver.com>
2023-01-28 16:50:47 +0900

    Delete game function

--- app.js
+++ app.js
@@ -64,33 +64,6 @@ function displayScore(winner){
     //whenever reach 5scores.. stop it
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let result;
-
-        let playerInput = prompt("Rock, Siccors, Paper!");
-        let computerInput = getComputerChoice()
-        result = playRound(playerInput, computerInput);
-        if(result === "player"){
-            playerScore++;
-            console.log(`You win, ${playerInput.toLowerCase()} beats ${computerInput}`)
-        }else if(result === "computer"){
-            computerScore++;
-            console.log(`You lose, ${computerInput} beats ${playerInput.toLowerCase()}`)
-        }else{
-            console.log(`The game ended in a tie, ${playerInput.toLowerCase()} beats ${computerInput}`)
-        }
-
-    if(playerScore > computerScore){
-        console.log("@winner is player")
-    }else if(playerScore < computerScore){
-        console.log("@winner is computer")
-    }else{
-        console.log("@both are winner!")
-    }
-}
-
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(btn => {
